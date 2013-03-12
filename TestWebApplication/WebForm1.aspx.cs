9f822a520c814c2c65f1a7b5b180a8da40808816
gitanjali <gitanjalig@xpanxion.co.in>
2013-03-12 16:51:52 +0530

    error code

--- TestWebApplication/WebForm1.aspx.cs
+++ TestWebApplication/WebForm1.aspx.cs
@@ -9,9 +9,33 @@ namespace TestWebApplication
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        static double zero;
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+            try
+            {
+                Response.Write(0 / zero == double.NaN);
+                Response.Write(0 / zero != double.NaN);
+                Response.Write(double.IsNaN(0 / zero));
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+    }
+
+    public class Class1
+    {
+        // Violation occurs because the finalizer is empty.
+        ~Class1()
+        {
         }
     }
+
+
 }
